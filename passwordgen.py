6a5598754da1c7ab701ad7bcf0c61173eb5148c5
Nathanael Budge <nathanaelcbudge@gmail.com>
2022-09-30 13:46:26 -0600

    updated signupuser.cmd and server.js

--- passwordgen.py
+++ passwordgen.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+N=16
 N=int(input("How many digits should this code have? "))
 
 res = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(N))
