2ce4c585319e4396320e9246194255ef1c83c0d2
Nathanael Budge <nathanaelcbudge@gmail.com>
2022-10-24 13:29:48 -0600

    added ip addr to signin and signupuser cmd files

--- signupuser.cmd
+++ signupuser.cmd
@@ -1,1 +1,1 @@
-curl -v -d "@newuser.json" -X POST -H "Content-Type:application/json" http://localhost:3000/user
+curl -v -d "@newuser.json" -X POST -H "Content-Type:application/json" https://34.173.72.222
